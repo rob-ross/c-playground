af38b6360c5e4e7cbcce729cdc2aa5357b09f0bd
killeroonie <rob.ross@gmail.com>
2026-02-03 16:41:39 -0800

    completed problem set 4 assignment.Don't need generated output.wav file.

---
+++ week4/file_pointers.c
@@ -0,0 +1,72 @@
+//
+// Created by Rob Ross on 2/3/26.
+//
+
+// file pointers are FILE*.
+
+
+#include <stdio.h>
+// assumes file_name is a text file and not too large.
+// reads and prints one char at a time via fgetc and putchar.
+void display_file_fgetc(const char *file_name) {
+    if (!file_name) {
+        return;
+    }
+
+    // todo is there a method to tell you if the file exists? Or is a NULL return from fopen() the only clue?
+    FILE* file = fopen(file_name, "r");
+    if (!file) {
+        printf("FILE* NULL after: fopen('%s')\n", file_name);
+    }
+
+    // read and print one character at a time:
+    printf("Displaying contents of file: %s\n", file_name);
+    printf("---------------------------------------------\n");
+    int c = 0;
+    while ( (c = fgetc(file)) != EOF) {
+        putchar(c);
+    }
+
+    fclose(file);
+
+}
+// reads and prints a buffer at a time via fread
+void display_file_fread(const char *file_name) {
+    if (!file_name) {
+        return;
+    }
+    FILE* file = fopen(file_name, "r");
+    if (!file) {
+        printf("FILE* NULL after: fopen('%s')\n", file_name);
+    }
+    printf("Displaying contents of file: %s\n", file_name);
+    printf("---------------------------------------------\n");
+
+#define ROB_BUFFER_SIZE_ 1024
+    int buffer[ROB_BUFFER_SIZE_];
+
+    size_t read = 0L;
+
+    while (  (read = fread(buffer, sizeof(int), ROB_BUFFER_SIZE_ - 1, file)) ==  ROB_BUFFER_SIZE_ - 1) {
+        size_t chars_read = sizeof(int) * read;
+        buffer[ read ] = 0;
+        // printf("\nbuffer read %zu elements, %zu characters:\n%s", read, chars_read, (char*)buffer);
+        printf("%s", (char*)buffer);
+    }
+    // partial buffer
+    buffer[ read ] = 0;
+    printf("%s\n", (char*)buffer);
+#undef ROB_BUFFER_SIZE_
+
+    printf("EOF reached: %i, error: %i, perror: ", feof(file), ferror(file));
+
+    perror("Sample Error Msg"); //print error message to stderror
+
+    fclose(file);
+
+
+}
+
+int main(void) {
+    display_file_fread("pointers.c");
+}
--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -6,12 +6,34 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <tgmath.h>
+
+
+//make: clang volume.c -o volume.out
 
 // Number of bytes in .wav header
 const int HEADER_SIZE = 44;
 
+
+// multiples each member of the buffer by factor amount, rounding the result
+// modifies the buffer in-place.
+void apply_factor(int16_t *buffer, const size_t size, const double factor) {
+    for (int i = 0; i < size; ++i) {
+        buffer[i] = round(buffer[i] * factor); // can clip.
+    }
+}
+
+void handle_error(FILE* input, FILE* output, char *message) {
+    perror(message);
+    fclose(input);
+    fclose(output);
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
+
     // Check command-line arguments
     if (argc != 4)
     {
@@ -19,26 +41,72 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+//R"(input.wav)")  // what is a raw character literal?
+    if ( strcmp(argv[2], "input.wav") == 0) {
+        printf("Cannot write to 'input.wav'\n");
+        return 1;
+    }
+
     // Open files and determine scaling factor
     FILE *input = fopen(argv[1], "r");
     if (input == NULL)
     {
-        printf("Could not open file.\n");
+        printf("Could not open input file '%s'.\n", argv[1]);
         return 1;
     }
 
     FILE *output = fopen(argv[2], "w");
     if (output == NULL)
     {
-        printf("Could not open file.\n");
+        printf("Could not open output file '%s'.\n", argv[2]);
         return 1;
     }
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    //  Copy header from input file to output file
+    uint8_t header_buffer[HEADER_SIZE];
+    size_t read_size = fread(header_buffer, HEADER_SIZE, 1, input); // should == 1 for 1 element read
+    if (read_size == 0 || ferror(input) != 0) {
+        handle_error(input, output,"Error reading header");
+        return 1;
+    }
 
-    // TODO: Read samples from input file and write updated data to output file
+    size_t write_size = fwrite(header_buffer, HEADER_SIZE, 1, output);
+    if (write_size == 0 || ferror(output) != 0) {
+        handle_error(input, output,"Error writing header");
+        return 1;
+    }
+
+    // Read samples from input file and write updated data to output file
+
+    const size_t WAVE_BUFFER_SIZE = 1024;
+    int16_t wave_buffer[WAVE_BUFFER_SIZE]; // we probably want to read as signed int to handle clipping if factor causes overflow/underflow
+
+    while ( (read_size = fread(wave_buffer, sizeof(int16_t), WAVE_BUFFER_SIZE, input)) == WAVE_BUFFER_SIZE) {
+
+        // multiply buffer by factor
+        apply_factor(wave_buffer, read_size, factor);
+        //write modified buffer to output
+        write_size = fwrite(wave_buffer, sizeof(int16_t), read_size, output);
+        if ( write_size != read_size || ferror(output) != 0) {
+            handle_error(input, output,"Error writing output data.");
+            return 1;
+        }
+
+    }
+
+    // write remaining buffer
+
+    write_size = fwrite(wave_buffer, sizeof(int16_t), read_size, output);
+
+    // multiply by factor
+    apply_factor(wave_buffer, read_size, factor);
+
+    if ( write_size != read_size || ferror(output) != 0) {
+        handle_error(input, output,"Error writing output data.");
+        return 1;
+    }
 
     // Close files
     fclose(input);
