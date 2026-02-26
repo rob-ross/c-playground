ec5dd80f6858d7c0268bee65e1b0f9caabe5bcd2
killeroonie <rob.ross@gmail.com>
2026-02-25 18:17:04 -0800

    Added CTypeEnum, ctype_repr_str, misc documentation changes.

--- roblib/carray/carray_types.h
+++ roblib/carray/carray_types.h
@@ -4,6 +4,11 @@
 // Created by Rob Ross on 2/24/26.
 //
 
+/**
+*  This header defines macros get, set, repr.
+*  If you need to avoid them, #undef after include
+*/
+
 #pragma once
 
 #include "carray_template.h"
@@ -358,12 +363,12 @@
 /**
  * Returns the element of `array` at `index` in the `out` parameter. Returns CARRAY_OK on success, or an error value
  * in case of failure. If the method does not return CARRAY_OK, the `out` parameter is not changed.
- * @param n number of elements in `array`
+ * @param n size_t: number of elements in `array`
  * @param array the array from which to get the element
- * @param index the index position of the array. If greater than n-1, returns CARRAY_ERR_INDEX_OUT_OF_RANGE
+ * @param index size_t: the index position of the array. If greater than n-1, returns CARRAY_ERR_INDEX_OUT_OF_RANGE
  * @param out either the value at array[index], or unchanged if an error occurrs.
- * @return the value at element array[index] in the `out` parameter. This method returns CARRAY_OK on success
- * and an error code on failure. `out` is only modified on success.
+ * @return CArrayError: This method returns CARRAY_OK on success and copies array[index] to `out`.
+ * An error code on failure. `out` is only modified on success
  */
 #define get(N, A, I, O) \
     _Generic(((A)[0]), \
@@ -372,6 +377,16 @@
     )((N), (A), (I), (O))
 
 
+/**
+ * Similar to get(), but if that function returns an error code, the fallback value is returned in `out`.
+ * This method always returns CARRAY_OK.
+ * @param n size_t: number of elements in `array`
+ * @param array the array from which to get the element
+ * @param index size_t: the index position of the array. If greater than n-1, returns `fallback` in `out`
+ * @param out either the value at array[index], or `fallback` if error occurrs.
+ * @param fallback the value returned in `out` if an error occurrs.
+ * @return CArrayError: CARRAY_OK. Sets value of `out` to array[index] or uses the fallback value if an error occurred.
+ */
 #define get_or(N, A, I, O, F) \
     _Generic(((A)[0]), \
         CARRAY_ELEM_TYPES(CARRAY__GET_OR_CASE) \
@@ -382,10 +397,10 @@
  * Prints a representation of the array via printf, on the current line. Does not print a newline.
  * If `limit` is greater than 0, uses this to limit the number of displayed elements to MIN(n, limit).
  * If `message` is not null, uses this string as the output prefix instead of array element type.
- * @param n number of elements in the array
+ * @param n size_t: number of elements in the array
  * @param array the array to display
- * @param message optional, if null prefixes output with array element type, otherwise uses this as the prefix
- * @param limit if > 0, restricts output to the first MIN(n, limit) array elements. If 0, limits to DEFAULT_LIMIT items.
+ * @param message char*: optional, if null prefixes output with array element type, otherwise uses this as the prefix
+ * @param limit size_t: if > 0, restricts output to the first MIN(n, limit) array elements. If 0, limits to DEFAULT_LIMIT items.
  */
 #define repr(N, A, M, L) \
     _Generic(((A)[0]), \
@@ -396,11 +411,11 @@
 
 /**
  * Sets the value of array[index] to the `value` argument.
- * @param n number of elements in the array
+ * @param n size_t: number of elements in the array
  * @param array the array for which to set the value
- * @param index the index position of the array. If greater than n-1, returns CARRAY_ERR_INDEX_OUT_OF_RANGE
+ * @param index size_t: the index position of the array. If greater than n-1, returns CARRAY_ERR_INDEX_OUT_OF_RANGE
  * @param value the value to set at array[index]
- * @return CARRAY_OK on success, or error code on failure.
+ * @return CArrayError: CARRAY_OK on success, or error code on failure.
  */
 #define set(N, A, I, V) \
     _Generic(((A)[0]), \
